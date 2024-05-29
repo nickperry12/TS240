0306fa06aacea73bd44939b523ccd16e3a237e5f
Nick Perry <nick.perry12@outlook.com>
2024-05-29 16:42:38 -0700

    more practice

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,21 @@
-let x: number = 10;
-let y: number = 5;
-let result = x + y;
+type Vehicle = { make: string; model: string; year: number };
+type Motorcycle = Vehicle & { type: "motorcycle" };
+type Car = Vehicle & { type: "car"; doors: number };
+
+function isCar(vehicle: Vehicle | Car | Motorcycle): vehicle is Car {
+  return "type" in vehicle && "doors" in vehicle;
+}
+
+// Usage
+let myCar: Car = {
+  make: "Toyota",
+  model: "Camry",
+  year: 2021,
+  type: "car",
+  doors: 4,
+};
+
+if (isCar(myCar)) {
+  console.log(myCar.doors);
+}
+
