40771f9ad1ac58c45398cf14d6b249c62ba55cd8
Nick Perry <nick.perry12@outlook.com>
2024-05-30 02:00:04 -0700

    practice problems

--- src/test.ts
+++ src/test.ts
@@ -1,21 +1,55 @@
-type Vehicle = { make: string; model: string; year: number };
-type Motorcycle = Vehicle & { type: "motorcycle" };
-type Car = Vehicle & { type: "car"; doors: number };
+/*
+Write a function describeAnimal that takes an Animal object as a parameter and
+returns a description string. The Animal type is a discriminated union that can
+be either a dog or a bird.
 
-function isCar(vehicle: Vehicle | Car | Motorcycle): vehicle is Car {
-  return "type" in vehicle && "doors" in vehicle;
+You will need to provide an implementation of the Animal type that uses
+discriminated unions to handle the different cases. For dogs, it should return
+"name is a age year(s) old dog."; for birds, it should return "name is a bird
+with a wingspan cm wingspan."
+*/
+type Bird = {
+  kind: "bird";
+  name: string;
+  wingspan: number;
 }
 
-// Usage
-let myCar: Car = {
-  make: "Toyota",
-  model: "Camry",
-  year: 2021,
-  type: "car",
-  doors: 4,
-};
-
-if (isCar(myCar)) {
-  console.log(myCar.doors);
+type Dog = {
+  kind: "dog";
+  name: string;
+  age: number;
 }
+
+type Animal = 
+  | Dog
+  | Bird;
+
+const dog: Dog = {
+  kind: "dog",
+  name: "Tommy",
+  age: 4,
+}
+
+const bird: Bird = {
+  kind: "bird",
+  name: "Polly",
+  wingspan: 15,
+}
+
+function describeAnimal(animal: Animal): string {
+  let output: string;
+  switch (animal.kind) {
+    case "dog":
+      output =  `${animal.name} is ${animal.age} year(s) old dog.`;
+      break;
+    case "bird":
+      output = `${animal.name} is a bird with a ${animal.wingspan}cm wingspan.`;
+      break;
+  }
+
+  return output;
+}
+
+console.log(describeAnimal(dog));
+console.log(describeAnimal(bird));
 
